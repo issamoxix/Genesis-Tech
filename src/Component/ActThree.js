3a51ef1c6939ec01d6bfdb63d4e2a0b9d840df8b
Issamoxix <issamhaidaoui@gmail.com>
2020-11-06 00:43:28 +0100

    another commit

--- src/Component/ActThree.js
+++ src/Component/ActThree.js
@@ -5,18 +5,19 @@ import "./ActThree.css"
 function ActThree(){
     return (
         <div className="contact-container">
-            <div className="img">
-                
-            </div>
-            <div className="contact-body">
-                <h1>Contact Nous </h1>
-                <form className="contact-form">
-                    <input type="text" placeholder="Full Name" />
-                    <input type="text" placeholder="Email" />
-                    <input type="text" placeholder="Subject " />
-                    <textarea cols="30" rows="15"></textarea>
-                    <input type="submit" value="Send" />
-                </form>
+            <div className="hands">
+                <div className="contact-body">
+                    <h1>Contact Nous</h1>
+                    <div className="contact-form" >
+                        
+                        <input type="text" placeholder="Nome et Prenom" required />
+                        <input type="email" placeholder="Email" required />
+                        <input type="text" placeholder="Suject" required />
+                        <textarea rows="12" cols="10"></textarea>
+                        <input type="submit" value="Send" />
+
+                    </div>
+                </div>
             </div>
         </div>
     )
