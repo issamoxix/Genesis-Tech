f972ecbe04ae014aaab29ab3fd409b73656568ae
Issamoxix <issamhaidaoui@gmail.com>
2020-11-10 02:06:45 +0100

    update

--- src/Component/Cards.js
+++ src/Component/Cards.js
@@ -2,22 +2,16 @@ import React from 'react';
 import "./Cards.css"
 import Card from "./Card"
 function Cards(){
-   
+   const data = "Mollit tempor     minim velit. Occaecat elit irure dolor irure fugiat proident quis est laborum     "
     return (
         <>
 
-         <Card title="Service #1" p=" Mollit tempor     minim velit. Occaecat elit irure dolor irure fugiat proident quis est laborum laborum. Ea commodo ipsum consequat consequat ullamco culpa.
-" />
-       <Card title="Service #2" p=" Mollit tempor     minim velit. Occaecat elit irure dolor irure fugiat proident quis est laborum laborum. Ea commodo ipsum consequat consequat ullamco culpa.
-" />
- <Card title="Service #3" p=" Mollit tempor     minim velit. Occaecat elit irure dolor irure fugiat proident quis est laborum laborum. Ea commodo ipsum consequat consequat ullamco culpa.
-" />
- <Card title="Service #4" p=" Mollit tempor     minim velit. Occaecat elit irure dolor irure fugiat proident quis est laborum laborum. Ea commodo ipsum consequat consequat ullamco culpa.
-" />
- <Card title="Service #5" p=" Mollit tempor     minim velit. Occaecat elit irure dolor irure fugiat proident quis est laborum laborum. Ea commodo ipsum consequat consequat ullamco culpa.
-" />
- <Card title="Service #6" p=" Mollit tempor     minim velit. Occaecat elit irure dolor irure fugiat proident quis est laborum laborum. Ea commodo ipsum consequat consequat ullamco culpa.
-" />
+         <Card title="Service #1" p={data} />
+       <Card title="Service #2" p={data} />
+ <Card title="Service #3" p={data} />
+ <Card title="Service #4" p={data} />
+ <Card title="Service #5" p={data} />
+ <Card title="Service #6" p={data} />
         </>
     )
 }
